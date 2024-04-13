55aa2ac2e4ec1c84bc462d6395b62d55b41124b4
EmiliaKostrubała <kostrubala.em@gmail.com>
2024-04-13 14:24:52 +0200

    subpage post with comments

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, g
 from albums import albums_bp
 import requests
+import logging
+
+#przygotowanie loggera
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='c:\\temp\\TiOK.log', encoding='utf-8', level=logging.DEBUG, format='%(levelname)s:%(asctime)s %(message)s', datefmt='%T')
 
 app = Flask(__name__)
 app.register_blueprint(albums_bp)
@@ -19,8 +24,13 @@ def home():
 def post(post_id):
     response = requests.get('https://jsonplaceholder.typicode.com/posts/%s'%(post_id))
     post = response.json()
+    response = requests.get('https://jsonplaceholder.typicode.com/users/%s' % (post["userId"]))
+    user=response.json()
+    response = requests.get('https://jsonplaceholder.typicode.com/comments?postId=%s' %(post_id))
+    comments=response.json()
 
-    return render_template('post.html', post=post)
+    return render_template('post.html', post=post, user=user, comments=comments)
+
 
 @app.route('/albums')
 def albums():
