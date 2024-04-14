c28b8419244cc69a45e557c458b2a00c130ace7b
xpatxj <patrycjagrzesiak100@gmail.com>
2024-04-15 01:05:58 +0200

    added two tests

--- tests/repository_tests.py
+++ tests/repository_tests.py
@@ -1,8 +1,11 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 import repository
 
 class TestRepository(unittest.TestCase):
-
 
     def test_posts_length(self):
         posts = repository.get_posts()
@@ -20,6 +23,7 @@ class TestRepository(unittest.TestCase):
         post = repository.get_post(1)
         self.assertNotEqual(len(post["user"]), 0)
         self.assertNotEqual(len(post["comments"]), 0)
+
     def test_post_with_wrong_id(self):
         post = repository.get_post("niepoprawny argument")
         self.assertEqual(len(post), 0)
@@ -35,9 +39,20 @@ class TestRepository(unittest.TestCase):
     def test_photos_with_wrong_id(self):
         photo = repository.get_photos("niepoprawny argument")
         self.assertEqual(len(photo), 0)
+
     def test_photos_with_not_existing_id(self):
         photo = repository.get_photos(-1)
         self.assertEqual(len(photo), 0)
+
+    def test_get_photos_non_empty(self):
+        photos = repository.get_photos(1)
+        self.assertTrue(all(len(photo) > 0 for photo in photos))
+
+    def test_get_post_keys(self):
+        post = repository.get_post(1)
+        self.assertIn("post", post)
+        self.assertIn("user", post)
+        self.assertIn("comments", post)
 
 if __name__ == '__main__':
     unittest.main()
