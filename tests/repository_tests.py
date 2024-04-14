34feff29eec0e9c4168cf242ab8f6f87e90f4ece
xpatxj <patrycjagrzesiak100@gmail.com>
2024-04-15 01:16:38 +0200

    added more tests

--- tests/repository_tests.py
+++ tests/repository_tests.py
@@ -54,5 +54,38 @@ class TestRepository(unittest.TestCase):
         self.assertIn("user", post)
         self.assertIn("comments", post)
 
+    def test_get_post_type(self):
+        post = repository.get_post(1)
+        self.assertIsInstance(post, dict)
+
+    def test_get_albums_type(self):
+        albums = repository.get_albums()
+        self.assertIsInstance(albums, list)
+
+    def test_get_photos_type(self):
+        photos = repository.get_photos(1)
+        self.assertIsInstance(photos, list)
+
+    def test_get_post_user_keys(self):
+        post = repository.get_post(1)
+        self.assertIn("id", post["user"])
+        self.assertIn("name", post["user"])
+        self.assertIn("username", post["user"])
+
+    def test_get_post_comments_keys(self):
+        post = repository.get_post(1)
+        for comment in post["comments"]:
+            self.assertIn("id", comment)
+            self.assertIn("name", comment)
+            self.assertIn("email", comment)
+
+    def test_get_photos_keys(self):
+        photos = repository.get_photos(1)
+        for photo in photos:
+            self.assertIn("id", photo)
+            self.assertIn("title", photo)
+            self.assertIn("url", photo)
+            self.assertIn("thumbnailUrl", photo)
+
 if __name__ == '__main__':
     unittest.main()
